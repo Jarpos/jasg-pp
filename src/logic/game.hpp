660ed474d03bff3c470718ace256fe56d7c708f3
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-05-13 19:22:30 +0200

    style: move stuff

--- src/logic/game.hpp
+++ src/logic/game.hpp
@@ -5,13 +5,19 @@
 namespace logic
 {
 
+enum class TileType : int {
+    none,
+    apple,
+    head,
+    snake,
+};
+
 enum class MoveDirection : int {
     left = 1 << 0,
     right = 1 << 1,
     up = 1 << 2,
     down = 1 << 3,
 };
-enum class TileType : int { none, apple, head, snake };
 
 class Game
 {
