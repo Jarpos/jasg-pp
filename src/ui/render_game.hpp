44215b787e0bd518cceb314402d18c4b03513e42
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-05-14 18:23:13 +0200

    ref: update rendering for new saving of snake

--- src/ui/render_game.hpp
+++ src/ui/render_game.hpp
@@ -7,39 +7,33 @@
 namespace ui
 {
 
-void render_game(sf::RenderWindow& w, const logic::Game& b, int tilesize)
+void render_game(sf::RenderWindow& w, const logic::Game& g, int tilesize)
 {
-    const auto& tiles = b.getBoard();
-    for (int x = 0; x < tiles.size(); x++) {
-        for (int y = 0; y < tiles[x].size(); y++) {
+    // Draw board
+    for (int x = 0; x < g.GetSize(); x++) {
+        for (int y = 0; y < g.GetSize(); y++) {
             sf::RectangleShape r(sf::Vector2f(tilesize, tilesize));
             r.setPosition(x * tilesize, y * tilesize);
-            // r.setOutlineThickness(5);
 
-            switch (tiles[x][y]) {
-            case logic::TileType::apple:
-                r.setFillColor(sf::Color(0xAA, 0x00, 0x00));
-                break;
-
-            case logic::TileType::head:
-                r.setFillColor(sf::Color(0x00, 0xDD, 0x00));
-                break;
-
-            case logic::TileType::snake:
-                r.setFillColor(sf::Color(0x00, 0xAA, 0x00));
-                break;
-
-            case logic::TileType::none:
-                if ((x + y) % 2)
-                    r.setFillColor(sf::Color(0x80, 0x80, 0x80));
-                else
-                    r.setFillColor(sf::Color(0x2b, 0x2b, 0x2b));
-
-                break;
-            }
-
+            if ((x + y) % 2)
+                r.setFillColor(sf::Color(0x80, 0x80, 0x80));
+            else
+                r.setFillColor(sf::Color(0x2b, 0x2b, 0x2b));
             w.draw(r);
         }
+    }
+
+    // Draw snake & apple
+    sf::RectangleShape r(sf::Vector2f(tilesize, tilesize));
+    r.setFillColor(sf::Color(0xFF, 0x00, 0x00));
+    r.setPosition(g.GetApple().x * tilesize, g.GetApple().y * tilesize);
+    w.draw(r);
+
+    for (const auto& s : g.GetSnake()) {
+        sf::RectangleShape r(sf::Vector2f(tilesize, tilesize));
+        r.setFillColor(sf::Color(0x00, 0xAA, 0x00));
+        r.setPosition(s.x * tilesize, s.y * tilesize);
+        w.draw(r);
     }
 }
 
