67c31d36bcdb17843899f28bdcdded76e4aefa16
Jarpos <51173894+Jarpos@users.noreply.github.com>
2022-05-18 14:07:44 +0200

    ref: make coloring for snake a little more varried and nicer

--- src/ui/render_game.hpp
+++ src/ui/render_game.hpp
@@ -29,16 +29,22 @@ void render_game(sf::RenderWindow& w, const logic::Game& g, int tilesize)
     r.setPosition(g.GetApple().x * tilesize, g.GetApple().y * tilesize);
     w.draw(r);
 
+    int i = 0;
     for (const auto& s : g.GetSnake()) {
         sf::RectangleShape r(sf::Vector2f(tilesize, tilesize));
-        r.setFillColor(sf::Color(0x00, 0xAA, 0x00));
         r.setPosition(s.x * tilesize, s.y * tilesize);
+
+        if (i++ % 2)
+            r.setFillColor(sf::Color(0x00, 0xAA, 0x00));
+        else
+            r.setFillColor(sf::Color(0x00, 0xBB, 0x00));
+
         w.draw(r);
     }
 
     sf::RectangleShape head(sf::Vector2f(tilesize, tilesize));
-    head.setFillColor(sf::Color(0x00, 0xFF, 0x00));
-    head.setPosition(g.GetSnake().at(0).x * tilesize, g.GetSnake().at(0).y * tilesize);
+    head.setFillColor(sf::Color(0x00, 0xDD, 0x00));
+    head.setPosition(g.GetSnake()[0].x * tilesize, g.GetSnake()[0].y * tilesize);
     w.draw(head);
 }
 
